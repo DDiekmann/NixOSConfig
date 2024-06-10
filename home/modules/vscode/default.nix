8786ff4f35e035cc0e8959dcaf5b3ee247a5cc55
d <d.d@d.d>
2024-06-10 13:39:25 +0200

    Add a bunch of vscode extensions

--- home/modules/vscode/default.nix
+++ home/modules/vscode/default.nix
@@ -12,14 +12,50 @@
         enable = true;
         package = pkgs.vscode;
         extensions = with pkgs.vscode-extensions; [
+          # git
           mhutchie.git-graph
-          foxundermoon.shell-format
+          eamodio.gitlens
+
+          # organization
+          gruntfuggly.todo-tree
+
+          #code quality
+          shardulm94.trailing-spaces
+
+          # devops
           ms-azuretools.vscode-docker
           ms-vscode-remote.remote-containers
           ms-vscode-remote.remote-ssh
           github.vscode-github-actions
+
+          # nix
+          arrterian.nix-env-selector
+          jnoortheen.nix-ide
+          mkhl.direnv
+
+          # languages
+          foxundermoon.shell-format
+          ms-python.python
+          yzhang.markdown-all-in-one
+
+          # assistants
+          # Codium.codium
+          # GitHub.copilot
+          # GitHub.copilot-chat
+          # TabNine.tabnine-vscode
+
+          # visuals
           vscode-icons-team.vscode-icons
+          catppuccin.catppuccin-vsc
+
+          # misc
+          # pnp.polacode
+          humao.rest-client
         ];
+        userSettings = {
+          editor.fontFamily = "'JetBrainsMono Nerd Font Mono'";
+          terminal.integrated.fontFamily = "'JetBrainsMono Nerd Font Mono'";
+        };
       };
 
       direnv = {
